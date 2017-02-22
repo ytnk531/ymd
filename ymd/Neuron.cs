d1bfde00d567379ad86d972b836d2a6eb5215669
Shota Sano <sphere.is.ring@gmail.com>
2017-02-22 23:03:12 +0900

    add Neuron Class

---
+++ ymd/Neuron.cs
@@ -0,0 +1,32 @@
+﻿using System;
+public delegate double ActivationFunc(double a);
+namespace ymd
+{
+	//delegate double ActivationFunc(double[] inputs);
+	public class Neuron
+	{
+		double[] weights;
+		double bias;
+		ActivationFunc actFunc;
+		public Neuron(int inputNums, ActivationFunc ActFunc, double[] weights, double bias)
+		{
+			//Double[] weights = new Double[inputNums];
+			this.weights= weights;
+			this.actFunc = ActFunc;
+			this.bias = bias;
+		}
+
+		public double compute(double[] inputs)
+		{
+			double a = 0;
+			for (var i = 0; i < inputs.Length; i++)
+			{
+				a += inputs[i] * weights[i];
+			}
+			a += bias;
+			return actFunc(a);
+		}
+
+
+	}
+}
